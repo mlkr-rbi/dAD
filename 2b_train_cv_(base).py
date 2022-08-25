de68883bd9449f6967f27bacf7faecdec79da824
davoors <75166378+davoors@users.noreply.github.com>
2022-08-25 13:00:15 +0200

    Update 2b_train_cv_(base).py

--- 2b_train_cv_(base).py
+++ 2b_train_cv_(base).py
@@ -10,12 +10,16 @@ SEED=239
 random.seed(SEED)
 np.random.seed(SEED)
 
+
+# A)
 # LOAD DATA
 train_00 = pd.read_csv('./sckba/train_00.csv')
 
 # Subset data on proper traning and calibration set
 calib = train_00.sample(n=4000, random_state=SEED)
 train_00 = train_00.drop(calib.index)
+train_00 = train_00.reset_index()
+train_00 = train_00.iloc[:,1:]
 
 # XGBOOST
 X_train, y_train =  train_00.iloc[:,3:], train_00.iloc[:,2]
@@ -34,7 +38,28 @@ model = xgb.XGBRegressor(colsample_bytree = 0.3, learning_rate = 0.1, max_depth 
 cv_preds = []
 for i in range(0,10):
     cv_preds.append(cross_val_predict(model, np.asarray(X_train), np.asarray(y_train), cv=cvx, method='predict', n_jobs=-1, verbose=1))
+   
 
-# SAVE FILE
-cv_predictions = pd.DataFrame(cv_preds).T
-cv_predictions.to_csv('./output/cv_predictions_BASELINE.csv')
+
+# B)
+# TRAIN ERROR MODEL
+cv_tr = pd.DataFrame(cv_preds).T
+cv_tr_mean = (cv_tr.iloc[:,0] + cv_tr.iloc[:,1] + cv_tr.iloc[:,2] + cv_tr.iloc[:,3] + cv_tr.iloc[:,4] + cv_tr.iloc[:,5] + cv_tr.iloc[:,6] + cv_tr.iloc[:,7] + cv_tr.iloc[:,8] + cv_tr.iloc[:,9]) / 10
+
+# XGBOOST
+X_train, y_train = train_00.iloc[:,3:], abs(cv_tr_mean - train_00.iloc[:,2])
+
+# CLEAN COLUMN NAMES
+# A) EDIT COLNAMES - TRAIN
+X_train.columns = X_train.columns.str.replace('[','')
+X_train.columns = X_train.columns.str.replace(']','')
+X_train.columns = X_train.columns.str.replace('<','')
+
+
+# TRAIN MODEL
+xg_reg_ERR = xgb.XGBRegressor(colsample_bytree = 0.3, learning_rate = 0.1, max_depth = 10, alpha = 10, n_estimators = 500, n_jobs=-1, random_state=SEED)
+xg_reg_ERR.fit(X_train, y_train)
+
+
+# SAVE MODEL!
+pickle.dump(xg_reg_ERR, open('./output/xg_reg_ERR_baseline.pkl', 'wb'))
