354ff05a77b89bd12e54845f95000998205e0367
davoors <75166378+davoors@users.noreply.github.com>
2022-08-25 10:55:23 +0200

    data path update

--- 5b_CP_baseline.py
+++ 5b_CP_baseline.py
@@ -1,159 +1,161 @@
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.ensemble import RandomForestRegressor
-from nonconformist.cp import IcpRegressor
-from nonconformist.nc import NcFactory
-from scoring_metrics import rmse, mse, ci
-import random 
-from sklearn.model_selection import train_test_split
-import xgboost as xgb
-from math import exp, log
+
+sx = ['S1', 'S2', 'S3', 'S4']
+modes = ['NN', 'CV']
+
+i_space =  pd.read_csv('./sckba/i_space.csv')
+model = pickle.load(open('./output/xgb_reg.pkl','rb'))
 
 
-#datasets = ['BindingDB_KI', 'ChEMBL_KI', 'Davis_KI', 'KIBA_KI', 'DTC_GPCR', 'DTC_SSRI']
-datasets = ['Davis_KI', 'DTC_SSRI']
+for s in sx:
 
-for data in datasets:
-    
-    # Load common files
-    data_tr =  pd.read_csv('./Benchmark/' + data + '/' + data + '_train.csv', index_col='Unnamed: 0')
-    data_ts =  pd.read_csv('./Benchmark/' + data + '/' + data + '_test.csv', index_col='Unnamed: 0')
-    ts_C =  pd.read_csv('./Benchmark/' + data + '/ts_C_space.csv', index_col='Unnamed: 0')
-    ts_T =  pd.read_csv('./Benchmark/' + data + '/ts_T_space.csv', index_col='Unnamed: 0')
-    i_space = pd.read_csv('./Benchmark/' + data + '/i_space.csv', index_col='Unnamed: 0')
-    train_nc = pd.read_csv('./Benchmark/' + data + '/train_nc.csv')
-    model = pickle.load(open('./Benchmark/' + data + '/xgb_ERR_model_(base).pkl','rb'))
+    # Load data and model
+    data_ts =  pd.read_csv('./sckba/test_' + s + '.csv')
+    ts_C =  pd.read_csv('./sckba/' + s + '_C.csv')
+    ts_T =  pd.read_csv('./sckba/' + s + '_T.csv')
 
-    # Subset data on proper traning and calibration set
-    SEED=239
-    random.seed(SEED)
-    np.random.seed(SEED)
-    calib = data_tr.sample(n=4000, random_state=SEED)
-    data_tr = data_tr.drop(calib.index)
-
-    # Prepare data (Training)
-    X_tr, y_tr = data_tr.iloc[:,3:], data_tr.iloc[:,2]
-    X_tr.columns = X_tr.columns.str.replace('[','')
-    X_tr.columns = X_tr.columns.str.replace(']','')
-    X_tr.columns = X_tr.columns.str.replace('<','')
-
-    # Prepare data (Calibration)
-    X_cal, y_cal = calib.iloc[:,3:], calib.iloc[:,2]
-    X_cal.columns = X_cal.columns.str.replace('[','')
-    X_cal.columns = X_cal.columns.str.replace(']','')
-    X_cal.columns = X_cal.columns.str.replace('<','')
-    
-    # Prepare data (Test)
+    # Prepare data for prediction
     X_ts, y_ts = data_ts.iloc[:,3:], data_ts.iloc[:,2]
-    X_ts.columns = X_ts.columns.str.replace('[','')
-    X_ts.columns = X_ts.columns.str.replace(']','')
-    X_ts.columns = X_ts.columns.str.replace('<','')
-
-
-    # TRAIN AND CALIBRATE
-    model_xgb = xgb.XGBRegressor(colsample_bytree = 0.3, learning_rate = 0.1, max_depth = 10, alpha = 10, n_estimators = 500, n_jobs=-1, random_state=SEED)
-    nc = NcFactory.create_nc(model_xgb)
-    icp = IcpRegressor(nc)
-
-    # Fit the data and calibrate
-    icp.fit(X_tr.to_numpy(), y_tr.to_numpy())
-    icp.calibrate(X_cal.to_numpy(), y_cal.to_numpy())
+    X_ts.columns = X_ts.columns.str.replace('[','', regex=True)
+    X_ts.columns = X_ts.columns.str.replace(']','', regex=True)
+    X_ts.columns = X_ts.columns.str.replace('<','', regex=True)
     
-    # Predict with model
-    ts_prediction_75 = icp.predict(X_ts.to_numpy(), significance=0.25)
-    ts_prediction_80 = icp.predict(X_ts.to_numpy(), significance=0.20)
-    ts_prediction_85 = icp.predict(X_ts.to_numpy(), significance=0.15)
-    ts_prediction_90 = icp.predict(X_ts.to_numpy(), significance=0.10)
-    ts_prediction_95 = icp.predict(X_ts.to_numpy(), significance=0.05)
-    ts_prediction_99 = icp.predict(X_ts.to_numpy(), significance=0.01)
+    # k, q sizes
+    k = 250; q = 25
+
+    for m in modes:
+        if m  == 'NN':
+
+            # Non-conformity set
+            ts_nc = data_ts.iloc[:,0:3]
+            ts_nc['y_pred'] = model.predict(X_ts)
+            ts_nc['abs_err'] = abs(ts_nc['affinity'] - ts_nc['y_pred'])
+
+            c_mean_sims = []
+            t_mean_sims = []
+            ts_conf_sizes = []
+                
+            for i in range(len(ts_nc)):
+                
+                # Top ranking compounds in the traning space
+                c_k_sim = ts_C.loc[:, ts_C.columns == ts_nc.loc[i,'SMILES']].sort_values(by=ts_nc.loc[i,'SMILES'], ascending=False)[0:k]
+                c_k_ids = ts_C[ts_C.index.isin(ts_C.loc[:, ts_C.columns == ts_nc.loc[i,'SMILES']].sort_values(by=ts_nc.loc[i,'SMILES'], ascending=False)[0:k].index)]['SMILES']
+                c_k = pd.merge(c_k_ids, c_k_sim, left_index=True, right_index=True)
+
+                # Top ranking kinases in the traning space
+                t_q_sim = ts_T.loc[:, ts_T.columns == ts_nc.loc[i,'Uniprot']].sort_values(by=ts_nc.loc[i,'Uniprot'], ascending=False)[0:q]
+                t_q_ids = ts_T[ts_T.index.isin(ts_T.loc[:, ts_T.columns == ts_nc.loc[i,'Uniprot']].sort_values(by=ts_nc.loc[i,'Uniprot'], ascending=False)[0:q].index)]['Uniprot']
+                t_q = pd.merge(t_q_ids, t_q_sim, left_index=True, right_index=True)
+                
+                # Find the interaction landscape of extracted conformity subspaces
+                conf_region = pd.merge(c_k_ids, i_space, left_on='SMILES', right_on='SMILES')
+                conf_region = conf_region.loc[:, conf_region.columns.isin(t_q_ids)]
+                conf_region.insert(0, "SMILES", c_k_ids.values)
+                conf_region = pd.melt(conf_region, id_vars=['SMILES'])
+                conf_region = conf_region.rename(columns={"variable": "Uniprot", "value": "affinity"})
+                conf_region = conf_region.dropna()
+                conf_region['mean_diff'] = abs(conf_region['affinity'] - conf_region['affinity'].mean())
+                conf_region['pred_diff'] = abs(conf_region['affinity'] - ts_nc.loc[i,'y_pred'])
+                
+                # Unite similarities and differences under one df
+                ts_nonconf = conf_region.merge(c_k, on="SMILES")
+                ts_nonconf = ts_nonconf.merge(t_q, on="Uniprot")
+                
+                # Retrieve mean similarities
+                c_mean_sims.append(ts_nonconf.iloc[:,5].mean())
+                t_mean_sims.append(ts_nonconf.iloc[:,6].mean())
+                
+                # Check conformity region sizes!
+                ts_conf_sizes.append(len(ts_nonconf))
+                
+                # Compute alpha scores
+                for j in range(len(ts_nonconf)):
+                    ts_nonconf.loc[j,'alpha_sc'] = (ts_nonconf[ts_nonconf['pred_diff'] <= ts_nonconf.loc[j,'mean_diff']].shape[0]) / ts_nonconf.shape[0]
+                
+                
+                # Locate smallest alpha for any confidence level
+                ts_nc.loc[i,'75_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.75,0.79)].mean_diff)
+                ts_nc.loc[i,'80_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.8,0.84)].mean_diff)
+                ts_nc.loc[i,'85_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.85,0.89)].mean_diff)
+                ts_nc.loc[i,'90_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.90,0.94)].mean_diff)
+                ts_nc.loc[i,'95_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.95,0.98)].mean_diff)
+                ts_nc.loc[i,'99_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.99,1)].mean_diff)
 
 
-    # Predict error
-    ts_ERR = model.predict(X_ts)
-    
-    # Define sensitivity param
-    gamma = 0; gamma_ = 0
-    if data == 'BindingDB_KI' | data == 'DTC_SSRI':
-        gamma=0; gamma_ = 0
-    elif data == 'ChEMBL_KI':
-        gamma=0.3; gamma_ = 0
-    else:
-        gamma=0.7; gamma_=0
+            # Write files
+            ts_conf_sizes_df = pd.DataFrame(ts_conf_sizes)
+            ts_conf_sizes_df.to_csv('./output/' + s + '_conf_sizes_NN.csv')
+            ts_nc.to_csv('./output/' + s + '_nonconf_NN.csv')
 
 
-    # Define nn params (k,q)
-    if data == 'Davis_KI':
-        k=25; q=25
-    elif data == 'DTC_SSRI':
-        k=250; q=10
-    else:
-        k=250; q=25
+        else:
+            # Assign CV predictions for training samples
+            data_tr =  pd.read_csv('./sckba/train_00.csv')
+            cv_tr = pd.read_csv('./output/cv_predictions.csv', index_col='Unnamed: 0')
+            cv_tr_mean = (cv_tr.iloc[:,0] + cv_tr.iloc[:,1] + cv_tr.iloc[:,2] + cv_tr.iloc[:,3] + cv_tr.iloc[:,4] + cv_tr.iloc[:,5] + cv_tr.iloc[:,6] + cv_tr.iloc[:,7] + cv_tr.iloc[:,8] + cv_tr.iloc[:,9]) / 10
 
-    # Nonconformity datasets
-    ts_nc = data_ts.iloc[:,0:3]
-        
-    for i in range(len(ts_nc)):
-        
-        # Top ranking compounds in the traning space
-        c_x_sim = ts_C.loc[:, ts_C.columns == ts_nc.loc[i,'SMILES']].sort_values(by=ts_nc.loc[i,'SMILES'], ascending=False)[0:k]
-        c_x_ids = ts_C[ts_C.index.isin(ts_C.loc[:, ts_C.columns == ts_nc.loc[i,'SMILES']].sort_values(by=ts_nc.loc[i,'SMILES'], ascending=False)[0:k].index)]['SMILES']
-        c_x_ids = pd.merge(c_x_ids, c_x_sim, left_index=True, right_index=True)
+            data_tr = data_tr.iloc[:,0:3]
+            data_tr['cv_pred'] = cv_tr_mean
 
-        # Top ranking kinases in the traning space
-        t_x_sim = ts_T.loc[:, ts_T.columns == ts_nc.loc[i,'TARGETS']].sort_values(by=ts_nc.loc[i,'TARGETS'], ascending=False)[0:q]
-        t_x_ids = ts_T[ts_T.index.isin(ts_T.loc[:, ts_T.columns == ts_nc.loc[i,'TARGETS']].sort_values(by=ts_nc.loc[i,'TARGETS'], ascending=False)[0:q].index)]['TARGETS']
-        t_x = pd.merge(t_x_ids, t_x_sim, left_index=True, right_index=True)
-        
-        # Find the interaction landscape of extracted conformity subspaces
-        conf_region = pd.merge(c_x_ids, i_space, left_on='SMILES', right_on='SMILES')
-        conf_region = conf_region.loc[:, conf_region.columns.isin(t_x_ids)]
-        conf_region.insert(0, "SMILES", c_x_ids.values)
-        conf_region = pd.melt(conf_region, id_vars=['SMILES'])
-        conf_region = conf_region.rename(columns={"variable": "TARGETS", "value": "affinity"})
-        conf_region = conf_region.dropna()
+            ts_nc = data_ts.iloc[:,0:3]
+            ts_nc['y_pred'] = model.predict(X_ts)
+            ts_nc['abs_err'] = abs(ts_nc['affinity'] - ts_nc['y_pred'])
 
-        # Unite similarities and differences under one df
-        ts_nonconf_ = conf_region.merge(c_x_ids, on="SMILES")
-        ts_nonconf_ = ts_nonconf_.merge(t_x, on="TARGETS")
-        
-        ts_sim_sum = (ts_nonconf_.iloc[:,3] + ts_nonconf_.iloc[:,4])/2
-        lambda_d = sum(ts_sim_sum) / train_nc['sum_dist_ct'].median()
-        ksi_s = ts_nonconf_['affinity'].std() / train_nc['affinity_std'].median()
-        
-        
-        # B) DECIDE ON PREDICTION INTERVALS
-        ts_nc.loc[i, 'y_pred'] = ts_prediction_90[i].mean()
-        ts_nc.loc[i, 'abs_err'] = abs(ts_nc['affinity'][i] - ts_nc['y_pred'][i])
-        ts_nc.loc[i, '75_CI'] = abs(ts_prediction_75[i][0] - ts_prediction_75[i][1])/2
-        ts_nc.loc[i, '80_CI'] = abs(ts_prediction_80[i][0] - ts_prediction_80[i][1])/2
-        ts_nc.loc[i, '85_CI'] = abs(ts_prediction_85[i][0] - ts_prediction_85[i][1])/2
-        ts_nc.loc[i, '90_CI'] = abs(ts_prediction_90[i][0] - ts_prediction_90[i][1])/2
-        ts_nc.loc[i, '95_CI'] = abs(ts_prediction_95[i][0] - ts_prediction_95[i][1])/2
-        ts_nc.loc[i, '99_CI'] = abs(ts_prediction_99[i][0] - ts_prediction_99[i][1])/2
-        
-        ts_nc.loc[i, '75_CI_err'] = (abs(ts_prediction_75[i][0] - ts_prediction_75[i][1])/2) / exp(log(abs(ts_ERR[i])))
-        ts_nc.loc[i, '80_CI_err'] = (abs(ts_prediction_80[i][0] - ts_prediction_80[i][1])/2) / exp(log(abs(ts_ERR[i])))
-        ts_nc.loc[i, '85_CI_err'] = (abs(ts_prediction_85[i][0] - ts_prediction_85[i][1])/2) / exp(log(abs(ts_ERR[i])))
-        ts_nc.loc[i, '90_CI_err'] = (abs(ts_prediction_90[i][0] - ts_prediction_90[i][1])/2) / exp(log(abs(ts_ERR[i])))
-        ts_nc.loc[i, '95_CI_err'] = (abs(ts_prediction_95[i][0] - ts_prediction_95[i][1])/2) / exp(log(abs(ts_ERR[i])))
-        ts_nc.loc[i, '99_CI_err'] = (abs(ts_prediction_99[i][0] - ts_prediction_99[i][1])/2) / exp(log(abs(ts_ERR[i])))
+            c_mean_sims = []
+            t_mean_sims = []
+            ts_conf_sizes = []
 
-        ts_nc.loc[i, '75_CI_dist'] = (abs(ts_prediction_75[i][0] - ts_prediction_75[i][1])/2)/ (gamma + lambda_d)
-        ts_nc.loc[i, '80_CI_dist'] = (abs(ts_prediction_80[i][0] - ts_prediction_80[i][1])/2)/ (gamma + lambda_d)
-        ts_nc.loc[i, '85_CI_dist'] = (abs(ts_prediction_85[i][0] - ts_prediction_85[i][1])/2)/ (gamma + lambda_d)
-        ts_nc.loc[i, '90_CI_dist'] = (abs(ts_prediction_90[i][0] - ts_prediction_90[i][1])/2)/ (gamma + lambda_d)
-        ts_nc.loc[i, '95_CI_dist'] = (abs(ts_prediction_95[i][0] - ts_prediction_95[i][1])/2)/ (gamma + lambda_d)
-        ts_nc.loc[i, '99_CI_dist'] = (abs(ts_prediction_99[i][0] - ts_prediction_99[i][1])/2)/ (gamma + lambda_d)
-        
-        ts_nc.loc[i, '75_CI_std'] = (abs(ts_prediction_75[i][0] - ts_prediction_75[i][1])/2)/ (gamma_ + ksi_s)
-        ts_nc.loc[i, '80_CI_std'] = (abs(ts_prediction_80[i][0] - ts_prediction_80[i][1])/2)/ (gamma_ + ksi_s)
-        ts_nc.loc[i, '85_CI_std'] = (abs(ts_prediction_85[i][0] - ts_prediction_85[i][1])/2)/ (gamma_ + ksi_s)
-        ts_nc.loc[i, '90_CI_std'] = (abs(ts_prediction_90[i][0] - ts_prediction_90[i][1])/2)/ (gamma_ + ksi_s)
-        ts_nc.loc[i, '95_CI_std'] = (abs(ts_prediction_95[i][0] - ts_prediction_95[i][1])/2)/ (gamma_ + ksi_s)
-        ts_nc.loc[i, '99_CI_std'] = (abs(ts_prediction_99[i][0] - ts_prediction_99[i][1])/2)/ (gamma_ + ksi_s)
-        
-        #ts_nc.loc[i, 'gamma'] = gamma
+            for i in range(len(ts_nc)):
+                
+                # Top ranking compounds in the traning space
+                c_k_sim = ts_C.loc[:, ts_C.columns == ts_nc.loc[i,'SMILES']].sort_values(by=ts_nc.loc[i,'SMILES'], ascending=False)[0:k]
+                c_k_ids = ts_C[ts_C.index.isin(ts_C.loc[:, ts_C.columns == ts_nc.loc[i,'SMILES']].sort_values(by=ts_nc.loc[i,'SMILES'], ascending=False)[0:k].index)]['SMILES']
+                c_k = pd.merge(c_k_ids, c_k_sim, left_index=True, right_index=True)
 
+                # Top ranking kinases in the traning space
+                t_q_sim = ts_T.loc[:, ts_T.columns == ts_nc.loc[i,'Uniprot']].sort_values(by=ts_nc.loc[i,'Uniprot'], ascending=False)[0:q]
+                t_q_ids = ts_T[ts_T.index.isin(ts_T.loc[:, ts_T.columns == ts_nc.loc[i,'Uniprot']].sort_values(by=ts_nc.loc[i,'Uniprot'], ascending=False)[0:q].index)]['Uniprot']
+                t_q = pd.merge(t_q_ids, t_q_sim, left_index=True, right_index=True)
+                
+                # Find the interaction landscape of extracted conformity subspaces
+                conf_region = pd.merge(c_k_ids, i_space, left_on='SMILES', right_on='SMILES')
+                conf_region = conf_region.loc[:, conf_region.columns.isin(t_q_ids)]
+                conf_region.insert(0, "SMILES", c_k_ids.values)
+                conf_region = pd.melt(conf_region, id_vars=['SMILES'])
+                conf_region = conf_region.rename(columns={"variable": "Uniprot", "value": "affinity"})
+                conf_region = conf_region.dropna()
+                conf_region = pd.merge(conf_region, data_tr, on=["SMILES", "Uniprot", "affinity"])
 
-    ts_nc.to_csv('~/Benchmark/' + data +'/output/ts_nonconf_BASE_' + data + '.csv')
+                conf_region['cv_diff'] = abs(conf_region['affinity'] - conf_region['cv_pred'])
+                conf_region['pred_diff'] = abs(conf_region['affinity'] - ts_nc.loc[i,'y_pred'])
+                conf_region = conf_region.drop('cv_pred', 1)
+
+                # Unite similarities and differences under one df
+                ts_nonconf = conf_region.merge(c_k, on="SMILES")
+                ts_nonconf = ts_nonconf.merge(t_q, on="Uniprot")
+                
+                # Retrieve mean similarities
+                c_mean_sims.append(ts_nonconf.iloc[:,5].mean())
+                t_mean_sims.append(ts_nonconf.iloc[:,6].mean())
+                
+                # Check conformity region sizes!
+                ts_conf_sizes.append(len(ts_nonconf))
+                
+                for j in range(len(ts_nonconf)):
+                    ts_nonconf.loc[j,'alpha_sc'] = (ts_nonconf[ts_nonconf['pred_diff'] <= ts_nonconf.loc[j,'cv_diff']].shape[0]) / ts_nonconf.shape[0]
+                
+                
+                # Locate smallest alpha for any confidence level
+                ts_nc.loc[i,'75_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.75,0.79)].cv_diff)
+                ts_nc.loc[i,'80_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.8,0.84)].cv_diff)
+                ts_nc.loc[i,'85_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.85,0.89)].cv_diff)
+                ts_nc.loc[i,'90_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.90,0.94)].cv_diff)
+                ts_nc.loc[i,'95_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.95,0.98)].cv_diff)
+                ts_nc.loc[i,'99_CI'] = np.min(ts_nonconf[ts_nonconf['alpha_sc'].between(0.99,1)].cv_diff)
+
+            # Write files
+            ts_conf_sizes_df = pd.DataFrame(ts_conf_sizes)
+            ts_conf_sizes_df.to_csv('./output/' + s + '_conf_sizes_CV.csv')
+            ts_nc.to_csv('./output/' + s + '_nonconf_CV.csv')
