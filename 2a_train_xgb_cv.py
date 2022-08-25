9d669ab3e19cd742ce3668d0f976f0540da56edc
davoors <75166378+davoors@users.noreply.github.com>
2022-08-25 10:48:25 +0200

    data path update

--- 2a_train_xgb_cv.py
+++ 2a_train_xgb_cv.py
@@ -1,11 +1,10 @@
 import xgboost as xgb
 from sklearn.model_selection import cross_val_predict, KFold
-import pickle
 import pandas as pd
 import numpy as np
 
 # LOAD DATA
-train_00 = pd.read_csv('~/notebooks/PDB/DATA/CP_2022/data/train_00.csv')
+train_00 = pd.read_csv('./sckba/train_00.csv')
 
 # XGBOOST
 X_train, y_train =  train_00.iloc[:,3:], train_00.iloc[:,2]
@@ -23,8 +22,8 @@ model = xgb.XGBRegressor(colsample_bytree = 0.3, learning_rate = 0.1, max_depth 
 
 cv_preds = []
 for i in range(0,10):
-    cv_preds.append(cross_val_predict(model, np.asarray(X_train), np.asarray(y_train), cv=cvx, method='predict', n_jobs=40, verbose=1))
+    cv_preds.append(cross_val_predict(model, np.asarray(X_train), np.asarray(y_train), cv=cvx, method='predict', n_jobs=-1, verbose=1))
 
 # SAVE FILE
 cv_predictions = pd.DataFrame(cv_preds).T
-cv_predictions.to_csv('./scenarios/output/cv_predictions.csv')
+cv_predictions.to_csv('./output/cv_predictions.csv')
