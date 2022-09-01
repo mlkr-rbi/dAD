6f2312807758cb486ea967cf3a8f3d0433ecfb97
davoors <75166378+davoors@users.noreply.github.com>
2022-09-01 10:45:46 +0200

    Update 2_train_nc.py

--- benchmark/2_train_nc.py
+++ benchmark/2_train_nc.py
@@ -4,8 +4,7 @@ import pickle
 import random
 
 
-#datasets = ['BindingDB_KI', 'ChEMBL_KI', 'Davis_KI', 'KIBA_KI', 'DTC_GPCR', 'DTC_SSRI']
-datasets = ['Davis_KI', 'DTC_SSRI']
+datasets = ['BindingDB_KI', 'ChEMBL_KI', 'Davis_KI', 'KIBA_KI', 'DTC_GPCR', 'DTC_SSRI']
 
 for data in datasets:
 
