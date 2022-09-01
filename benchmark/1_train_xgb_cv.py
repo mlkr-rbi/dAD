e3c23c42df464764b9dc9ed1829aba0eeab04b3f
davoors <75166378+davoors@users.noreply.github.com>
2022-09-01 10:45:14 +0200

    Update 1_train_xgb_cv.py

--- benchmark/1_train_xgb_cv.py
+++ benchmark/1_train_xgb_cv.py
@@ -4,8 +4,7 @@ import pickle
 import pandas as pd
 import numpy as np
 
-#datasets = ['BindingDB_KI', 'ChEMBL_KI', 'Davis_KI', 'KIBA_KI', 'DTC_GPCR', 'DTC_SSRI']
-datasets = ['Davis_KI', 'DTC_SSRI']
+datasets = ['BindingDB_KI', 'ChEMBL_KI', 'Davis_KI', 'KIBA_KI', 'DTC_GPCR', 'DTC_SSRI']
 
 for data in datasets:
     # UPLOAD DATA
